25e27fef35003d0dddc218d3cad314fc85d7c586
Luca Ghislotti <ghislottiluca@gmail.com>
2020-05-25 11:32:15 +0200

    added handler lambda "get talk by url"

---
+++ AWS_lambda/handler_get_talk_by_url.js
@@ -0,0 +1,55 @@
+const connect_to_db = require('./db');
+
+// GET BY TALK HANDLER
+
+const talk = require('./Talk');
+
+module.exports.get_talk_by_url = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    let body = {}
+    if (event.body) {
+        body = JSON.parse(event.body)
+    }
+    // set default
+    if(!body.url) {
+        callback(null, {
+                    statusCode: 500,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Could not fetch the talks. Url is null.'
+        })
+    }
+    
+    if (!body.doc_per_page) {
+        body.doc_per_page = 10
+    }
+    
+    if (!body.page) {
+        body.page = 1
+    }
+    
+    connect_to_db().then(() => {
+        console.log('=> get_all talks');
+        
+        const ris=talk.find({url: body.url})
+        
+       talk.find({url: body.url})
+            .skip((body.doc_per_page * body.page) - body.doc_per_page)
+            .limit(body.doc_per_page)
+            .then(talks => {
+                    callback(null, {
+                        statusCode: 200,
+                        body: JSON.stringify(talks)
+                    })
+                }
+                
+            )
+            .catch(err =>
+                callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Could not fetch the talks.'
+                })
+            );
+    });
+};
